d7e27e3edac7d7657128bd84411124f38cc373d3
Will Rogers <wahrogers@gmail.com>
2025-12-25 20:23:54 +0000

    Replace hamburger icon with SVG.

--- docs/js/menu.js
+++ docs/js/menu.js
@@ -1,23 +1,24 @@
 /* Javascript for the hamburger menu. */
-var checkbox = document.querySelector("#toggle-menu");
+const checkbox = document.querySelector("#toggle-menu");
 console.log(checkbox);
-var background = document.querySelector("body");
+const background = document.querySelector("body");
 console.log(background);
 background.addEventListener("click", hideMenu, false);
-var ham = document.querySelector("#toggle-label");
-ham.addEventListener("click", showMenu, true);
+const menuIcon = document.querySelector("#menu-icon");
+menuIcon.addEventListener("click", showMenu, true);
 function hideMenu(e) {
-    if (e.target.id !== "toggle-menu") {
-        if (checkbox.checked === false) {
-            checkbox.checked = true;
-        }
+  console.log("hideMenu");
+  if (e.target.id !== "toggle-menu") {
+    if (checkbox.checked === false) {
+      checkbox.checked = true;
     }
-    document.body.style.overflow = "auto";
+  }
+  document.body.style.overflow = "auto";
 }
 function showMenu(e) {
-    if (checkbox.checked === true) {
-        checkbox.checked = false;
-    }
-    e.stopPropagation();
-    document.body.style.overflow = "hidden";
+  if (checkbox.checked === true) {
+    checkbox.checked = false;
+  }
+  e.stopPropagation();
+  document.body.style.overflow = "hidden";
 }
