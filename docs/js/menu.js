679437a218445e0eb67a2ae7e5678eada2d80096
Will Rogers <wahrogers@gmail.com>
2025-12-27 20:20:33 +0000

    Simpler logic for hamburger menu.

--- docs/js/menu.js
+++ docs/js/menu.js
@@ -1,24 +1,24 @@
 /* Javascript for the hamburger menu. */
-const checkbox = document.querySelector("#toggle-menu");
-console.log(checkbox);
 const background = document.querySelector("body");
-console.log(background);
 background.addEventListener("click", hideMenu, false);
 const menuIcon = document.querySelector("#menu-icon");
+const menu = document.querySelector("nav");
 menuIcon.addEventListener("click", showMenu, true);
-function hideMenu(e) {
-  console.log("hideMenu");
-  if (e.target.id !== "toggle-menu") {
-    if (checkbox.checked === false) {
-      checkbox.checked = true;
-    }
-  }
-  document.body.style.overflow = "auto";
-}
+
 function showMenu(e) {
-  if (checkbox.checked === true) {
-    checkbox.checked = false;
+  const menuVisible = !menu.classList.contains("hide-menu");
+  if (!menuVisible) {
+    // document.body.style.overflow = "auto";
+    menu.classList.remove("hide-menu");
   }
   e.stopPropagation();
-  document.body.style.overflow = "hidden";
+}
+
+function hideMenu(e) {
+  const menuVisible = !menu.classList.contains("hide-menu");
+  if (menuVisible) {
+    // document.body.style.overflow = "auto";
+    menu.classList.add("hide-menu");
+  }
+  e.stopPropagation();
 }
