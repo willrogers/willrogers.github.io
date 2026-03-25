150a555c48183e9c52115c2d5c0b5fa69bb84e88
Will Rogers <wahrogers@gmail.com>
2026-03-25 19:32:03 +0000

    Add Prettier check to GitHub Actions.

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,11 @@
-const { defineConfig } = require('@playwright/test');
+const { defineConfig } = require("@playwright/test");
 
 module.exports = defineConfig({
-  testDir: 'tests',
+  testDir: "tests",
   timeout: 30 * 1000,
   use: {
     headless: true,
-    baseURL: 'http://localhost:8080',
+    baseURL: "http://localhost:8080",
     viewport: { width: 1280, height: 720 },
   },
 });
